b88c8a77d8be20b6c357450606b46a09fd319557
Louis Tiao <louistiao@gmail.com>
2013-09-30 01:28:21 +1000

    fixedc

--- pollstagram/views.py
+++ pollstagram/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse(os.path.dirname(os.path.dirname(os.path.abspath(__file__)) + "HERRO from django, try out <a href='/admin/'>/admin/</a>\n")
+    return HttpResponse(os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "HERRO from django, try out <a href='/admin/'>/admin/</a>\n")
 
