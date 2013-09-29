2f2977aaf13aba9403bdbeb4ae0c0e2727187ccf
Petr Novak <petr.no@gmail.com>
2013-09-29 07:15:12 -0400

    POL-18 Enabled admin iface

--- pollstagram/settings.py
+++ pollstagram/settings.py
@@ -57,7 +57,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -117,7 +117,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
---
+++ pollstagram/views.py
@@ -0,0 +1,5 @@
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Hello from django, try out <a href='/admin/'>/admin/</a>\n")
+
