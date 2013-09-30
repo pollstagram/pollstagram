49a5c23f0fe3cea3f120097ac99f0324187a6dfa
Louis Tiao <louistiao@gmail.com>
2013-09-30 16:35:57 +1000

    set up for static files

--- pollstagram/views.py
+++ pollstagram/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 import os
 
 def home(request):
-    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    cur_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     files = [f for f in os.listdir(cur_dir)]
     return HttpResponse('\n'.join(files))
 
