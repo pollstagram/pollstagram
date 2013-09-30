b661cc82697abcd3cff39a2719a700932baa6073
Louis Tiao <louistiao@gmail.com>
2013-09-30 16:44:07 +1000

    now rendering templates

--- pollstagram/views.py
+++ pollstagram/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 import os
 
 def home(request):
-    cur_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    files = [f for f in os.listdir(cur_dir)]
-    return HttpResponse('\n'.join(files))
+    return render(request, 'base.html')
+    # cur_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # files = [f for f in os.listdir(cur_dir)]
+    # return HttpResponse('\n'.join(files))
 
