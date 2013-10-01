e28902afa366d8c269260dce34448e1d0bdd4b22
Louis Tiao <louistiao@gmail.com>
2013-10-02 03:12:48 +1000

    POL-2 using django-pagedown for markdown editor for creating polls/asking questions

--- poll/admin.py
+++ poll/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django import forms 
 from pagedown.widgets import AdminPagedownWidget
-from poll.models import *
+from poll.models import Tag, Question, Choice, Vote, Answer
+from forms import QuestionForm
+
+class QuestionAdmin(admin.ModelAdmin):
+    form = QuestionForm
 
 admin.site.register(Tag)
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
 admin.site.register(Vote)
 admin.site.register(Answer)
