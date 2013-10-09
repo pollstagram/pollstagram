4af648d323b3c6fb6ab3b6e5977844ca281095cd
Louis Tiao <louistiao@gmail.com>
2013-10-09 23:20:44 +1100

    configured admin

--- poll/admin.py
+++ poll/admin.py
@@ -1,13 +1,28 @@
 from django.contrib import admin
-from django import forms 
-from poll.models import Tag, Question, Choice, Vote, Answer
-from poll.forms import QuestionForm
+from poll.models import Question, Choice, Answer
+from poll.forms import QuestionForm, ChoiceForm
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    form = ChoiceForm
+    extra = 2
 
 class QuestionAdmin(admin.ModelAdmin):
     form = QuestionForm
+    readonly_fields = ('published_time',)
+    fieldsets = [
+        (None,               {'fields': ['content_markdown']}),
+        ('Date information', {'fields': ['published_time'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('content_markdown', 'published_time')
+    list_filter = ['published_time']
+    search_fields = ['content_markdown']
+    date_hierarchy = 'published_time'
 
-admin.site.register(Tag)
+class ChoiceAdmin(admin.ModelAdmin):
+    form = ChoiceForm
+
 admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
-admin.site.register(Vote)
+admin.site.register(Choice, ChoiceAdmin)
 admin.site.register(Answer)
